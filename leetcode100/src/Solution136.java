caac15f70ef2daa94fe63db7354414ced32801c0
zhiQiang <ntumcm@163.com>
2024-08-30 20:02:55 +0800

    update

---
+++ leetcode100/src/Solution136.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class Solution136 {
+    
+    @Test
+    public void test(){
+    }
+    public void singleNumber(int[] nums) {
+
+    }
+/*  只出现一次的数字
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。除了某个元素只出现一次以外，其余每个元素均出现两次。
+
+输入：nums = [2,2,1]
+输出：1
+
+输入：nums = [4,1,2,1,2]
+输出：4
+
+输入：nums = [1]
+输出：1
+ */
+}
---
+++ leetcode100/src/Solution139.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+import org.junit.Test;
+
+public class Solution139 {
+    
+    @Test
+    public void test(){
+    }
+    public void wordBreak(String s, List<String> wordDict) {
+
+    }
+/*  单词拆分
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+s 和 wordDict[i] 仅由小写英文字母组成.wordDict 中的所有字符串 互不相同.
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     注意，你可以重复使用字典中的单词。
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false 
+*/
+}
