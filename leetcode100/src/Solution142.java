9c1b98462b9894d746cbd777fc625ad41798f735
zhiQiang <ntumcm@163.com>
2024-09-11 20:08:28 +0800

    update

--- leetcode100/src/Solution142.java
+++ leetcode100/src/Solution142.java
@@ -1,13 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import org.junit.Test;
 
 public class Solution142 {
     @Test
     public void test(){
+        ListNode head=new ListNode(3);
+        ListNode node1=new ListNode(2);
+        ListNode node2=new ListNode(0);
+        ListNode node3=new ListNode(-4);
+        head.next=node1; node1.next=node2; node2.next=node3; node3.next=node1;
+        /*********************************/
+        ListNode head1=new ListNode(5);
+        ListNode node4=new ListNode(7);
+        ListNode node5=new ListNode(8);
+        head1.next=node4; node4.next=node5;
+        System.out.println(detectCycle(head).val);
+        System.out.println(detectCycle(head1));
     }
-    public void detectCycle(ListNode head) {
-        
+    public ListNode detectCycle(ListNode head) {
+        ArrayList<ListNode> arrayList=new ArrayList<>();
+        detectCycle(head,arrayList);
+       if(arrayList.isEmpty()) return null;
+       else return arrayList.get(0);
     }
-/*  环形链表 II
+    public void detectCycle(ListNode head,ArrayList<ListNode> arrayList) {
+        HashSet<ListNode> hashSet=new HashSet<>();
+        while (head!=null) {
+            if(!hashSet.contains(head)) hashSet.add(head);
+            else {
+                arrayList.add(head);
+                break;
+            }
+            head=head.next;
+        }
+    }
+    /*  环形链表 II
 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
 不允许修改 链表。pos 的值为 -1 或者链表中的一个有效索引.进阶：你是否可以使用 O(1) 空间解决此题？
--- leetcode100/src/Solution148.java
+++ leetcode100/src/Solution148.java
@@ -1,12 +1,85 @@
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class Solution148 {
     @Test
     public void test(){
+        ListNode148 head=new ListNode148(4);
+        ListNode148 node1=new ListNode148(2);
+        ListNode148 node2=new ListNode148(1);
+        ListNode148 node3=new ListNode148(3);
+        head.next=node1; node1.next=node2; node2.next=node3; 
+        ListNode148 node=sortList(head);
+        while (node!=null) {
+            System.out.println(node.val);
+            node=node.next;
+        }
     }
-    public void sortList(ListNode148 head) {
-
+    public ListNode148 sortList(ListNode148 head) {
+        //逐一读取node存入arraylist，按值排序（快排），按顺序构造新链表
+        ArrayList<ListNode148> alist=new ArrayList<>();
+        while (head!=null) {
+            alist.add(head);
+            head=head.next;
+        }
+        fastSort(alist,0,alist.size()-1);
+        for (int i = 0; i < alist.size()-1; i++) {
+            alist.get(i).next=alist.get(i+1);
+        }
+        return alist.get(0);
     }
+    private void fastSort(ArrayList<ListNode148> alist ,int left,int right) {
+        //选left,(left+right)/2,right三个元素，按值排序。排好之后，选中间的元素为分割点。
+        //通过交换，使得分割点左边的元素值都小于它，右边的都大于它。
+        int middle=(left+right)/2;
+        if(alist.get(left).val>alist.get(middle).val) swap(alist, left, middle);
+        if(alist.get(middle).val>alist.get(right).val) swap(alist, right, middle);
+        if(alist.get(left).val>alist.get(middle).val) swap(alist, left, middle);
+        int i=left,j=right,pivot=middle;
+        while (i<pivot && j>pivot) {
+            while (i<pivot && alist.get(i).val<=alist.get(pivot).val) {
+                i++;
+            }
+            while (j>pivot && alist.get(j).val>=alist.get(pivot).val) {
+                j--;
+            }
+            if(i<pivot && j>pivot) swap(alist, i, j);
+        }
+        if(i>=pivot){    //分割点左边全部交换完毕，检查右边的元素：（pivot,j)
+            for (int k = pivot+1; k < j; k++) {
+                if(alist.get(k).val<alist.get(pivot).val) {
+                    ListNode148 temp=alist.get(k);
+                    for (int l = k; l >pivot; l--) {
+                        alist.set(l, alist.get(l-1));
+                    }
+                    alist.set(pivot, temp);
+                    pivot++;
+                }
+            }
+        }
+        if (j<=pivot) {     // (i,pivot)
+            for (int k = i+1; k < pivot; k++) {
+                if (alist.get(k).val>alist.get(pivot).val) {
+                    ListNode148 temp=alist.get(k);
+                    for (int l = k; l < pivot; l++) {
+                        alist.set(l, alist.get(l+1));
+                    }
+                    alist.set(pivot, temp);
+                    pivot--;
+                }
+            }
+        }
+        fastSort(alist, left, pivot);   //
+        fastSort(alist, pivot+1, right);
+    }
+    private void swap(ArrayList<ListNode148> alist,int i,int j){
+        ListNode148 node1=new ListNode148();
+        node1=alist.get(i);
+        alist.set(i, alist.get(j));
+        alist.set(j, node1);
+    }
+}
 /*    排序链表
  * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
  * 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
@@ -19,4 +92,4 @@ public class Solution148 {
 输入：head = []
 输出：[]
 */
-}
+
