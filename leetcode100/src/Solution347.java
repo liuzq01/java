0293f7f5da5f0c5c07fc999384d6fcedd4ec0769
zhiQiang <ntumcm@163.com>
2024-10-30 20:14:26 +0800

    up

--- leetcode100/src/Solution347.java
+++ leetcode100/src/Solution347.java
@@ -1,11 +1,48 @@
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class Solution347 {
     @Test
     public void test(){
+        int[]nums={1,1,1,2,2,3};
+        System.out.println(Arrays.toString(topKFrequent(nums, 2)));
+        int[]nums1={1,1,1,3,3,6,6,8,6,6,14};
+        System.out.println(Arrays.toString(topKFrequent(nums1, 3)));
+        int[]nums2={1,6,100,24,6,100,6,24,100,6};
+        System.out.println(Arrays.toString(topKFrequent(nums2, 4)));
     }
-    public int[] topKFrequent(int[] nums, int k) {
-        return null;
+    public int[] topKFrequent(int[] nums, int k) {  // 计数法
+        //找最大最小值
+        int min=nums[0], max=nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]<min) min=nums[i];
+            if(nums[i]>max) max=nums[i];
+        }
+        //遍历并计数nums中各元素的个数
+        int[] arr=new int[max-min+1];
+        for (int i = 0; i <nums.length; i++) {
+            arr[nums[i]-min]++;
+        }
+        //通过nums遍历arr那些已计数的元素,找频率前 k 高的元素
+        int[] result=new int[k];
+        int m=0;
+        int maxOfarr=0;
+        int lastMax=(int) (Math.pow(2, 32)-1);  // int类型数据的上限
+        int temp=0;
+        while (k!=0) {
+            for (int i = 0; i < nums.length; i++) {
+                if (arr[nums[i]-min]>maxOfarr && arr[nums[i]-min]<lastMax) {    
+                    maxOfarr=arr[nums[i]-min];
+                    temp=nums[i];
+                }
+            }
+            result[m]=temp;    
+            lastMax=maxOfarr;
+            maxOfarr=0;
+            m++; k--;
+        }
+        return result;
     }
 }
 /*
