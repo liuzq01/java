877058c2b3efedbefe20a0c62374ab5d3b6fd519
liuzq <ntumcm@163.com>
2025-01-18 20:48:04 +0800

    update

--- leetcode100/src/Solution461.java
+++ leetcode100/src/Solution461.java
@@ -3,9 +3,21 @@ import org.junit.Test;
 public class Solution461 {
     @Test
     public void test(){
+        System.out.println(hammingDistance(1, 4));
+        System.out.println(hammingDistance(1, 3));
     }
     public int hammingDistance(int x, int y) {
-        return 0;
+        //对x 和 y 作异或运算，结果是z，再从z中找为'1'的位，计数
+        //z与'1'作与运算，只有最低位参与有效运算，高位被屏蔽；不断把z右移一位，重复这个过程即可
+        int z=x^y;
+        int count=0;
+        while (z!=0) {
+            if ((z&1)==1) {
+                count++;
+            }
+            z=z>>1;                
+        }
+        return count;
     }
 }
 /*
